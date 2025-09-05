d0f66ba16a81745c3e3f6e46178817154e6b658c
U202418250 <u202418250@upc.edu.pe>
2025-09-05 16:43:36 -0500

    feat(product): enhance ProductId value object with documentation and new methods

--- ACME.OOP/Procurement/Domain/Model/valueObjetcs/ProductId.cs
+++ ACME.OOP/Procurement/Domain/Model/valueObjetcs/ProductId.cs
@@ -1,9 +1,18 @@
 namespace ACME.OOP.Procurement.Domain.Model.valueObjetcs;
 
+/// <summary>
+/// value object representing a product identifier in the Procurement bounded context.
+/// </summary>
+
 public record ProductId
 {
     public Guid Id { get; init; }
     
+    /// <summary>
+    /// creates a new instance of <see cref="ProductId"/>
+    /// </summary>
+    /// <param name="id">The unique identifier for the product.</param>
+    /// <exception cref="ArgumentException">Thrown when the identifier is empty</exception>
     public ProductId(Guid id)
     {
         if (id == Guid.Empty)
@@ -11,5 +20,16 @@ public record ProductId
         Id = id;
     }
     
+    /// <summary>
+    /// generates a new <see cref="ProductId"/>
+    /// </summary>
+    /// <returns>A new instance of <see cref="ProductId"/></returns>
+    
     public static ProductId New() => new (Guid.NewGuid());
+    
+    /// <summary>
+    /// Returns a string representation of the product identifier.
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() => Id.ToString();
 }
