6bb00b197c593ec4311e3beb3fc88394be428549
U202418250 <u202418250@upc.edu.pe>
2025-09-05 17:13:57 -0500

    feat(supplier): enhance Supplier class with address type and improved documentation

--- ACME.OOP/SCM/Domain/model/Aggregates/Supplier.cs
+++ ACME.OOP/SCM/Domain/model/Aggregates/Supplier.cs
@@ -1,13 +1,16 @@
-namespace ACME.OOP.SCM.Domain.model.Aggregates;
+using ACME.OOP.Shared.Domain.Model.ValueObjects;
+
+namespace ACME.OOP.SCM.Domain.Model.Aggregates;
+
 /// <summary>
-/// Represents a supplier in the Supply Chain Management bounded context.
+/// Represents a supplier entity in the supply chain management bounded context.
 /// </summary>
-/// <param name="identifier">The unique identifier for the supplier</param>
-/// <param name="name">The  </param>
-/// <param name="address"></param>
-public class Supplier(string identifier, string name, string address)
+/// <param name="identifier">The unique identifier for the supplier></param>
+/// <param name="name">The name of the supplier></param>
+/// <param name="address">The address of the supplier></param>
+public class Supplier(string identifier, string name, Address address)
 {
     public string Identifier { get; } = identifier ?? throw new ArgumentNullException(nameof(identifier));
     public string Name { get; } = name ?? throw new ArgumentNullException(nameof(name));
-    public string Address { get; } = address ?? throw new ArgumentNullException(nameof(address));
+    public Address Address { get; } = address ?? throw new ArgumentNullException(nameof(address));
 }
